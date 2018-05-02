57a163bbe16ae52c9b59b4b770157f7e0426e3be
Daryl <17547337+dboggs95@users.noreply.github.com>
2018-05-02 03:45:42 +0000

    Update cache_migration.rb
    Rails 5.1 can figure out rollback on its own.

--- lib/generators/ratyrate/templates/cache_migration.rb
+++ lib/generators/ratyrate/templates/cache_migration.rb
@@ -1,5 +1,5 @@
 class CreateRatingCaches < ActiveRecord::Migration[5.1]
-  def up
+  def self.change
     create_table :rating_caches do |t|
       t.belongs_to :cacheable, :polymorphic => true
       t.float :avg, :null => false
@@ -9,9 +9,5 @@ class CreateRatingCaches < ActiveRecord::Migration[5.1]
     end
 
     add_index :rating_caches, [:cacheable_id, :cacheable_type]
-  end
-
-  def down
-    drop_table :rating_caches
   end
 end
