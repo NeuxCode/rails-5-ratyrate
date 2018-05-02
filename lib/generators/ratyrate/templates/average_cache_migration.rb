735d7667131ef319ff64f69afc149f2b32822133
Daryl <17547337+dboggs95@users.noreply.github.com>
2018-05-02 03:44:31 +0000

    Update average_cache_migration.rb
    Rails 5.1 can figure out the rollback operation on its own.

--- lib/generators/ratyrate/templates/average_cache_migration.rb
+++ lib/generators/ratyrate/templates/average_cache_migration.rb
@@ -1,5 +1,5 @@
 class CreateAverageCaches < ActiveRecord::Migration[5.1]
-  def up
+  def self.change
     create_table :average_caches do |t|
       t.belongs_to :rater
       t.belongs_to :rateable, :polymorphic => true
@@ -8,9 +8,5 @@ class CreateAverageCaches < ActiveRecord::Migration[5.1]
     end
 
     add_index :average_caches, [:rater_id, :rateable_id, :rateable_type]
-  end
-
-  def down
-    drop_table :average_caches
   end
 end
