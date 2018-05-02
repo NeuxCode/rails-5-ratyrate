f69aa49da6e5f0f0f53dde7aabfd884d3b501fee
Daryl <17547337+dboggs95@users.noreply.github.com>
2018-05-02 04:28:26 +0000

    Update average_cache_migration.rb
    Having self.change was incorrect.

--- lib/generators/ratyrate/templates/average_cache_migration.rb
+++ lib/generators/ratyrate/templates/average_cache_migration.rb
@@ -1,5 +1,5 @@
 class CreateAverageCaches < ActiveRecord::Migration[5.1]
-  def self.change
+  def change
     create_table :average_caches do |t|
       t.belongs_to :rater
       t.belongs_to :rateable, :polymorphic => true
