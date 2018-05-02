bc911aeaf858837ca36b0b3cf0dd74e52167e9bc
Daryl <17547337+dboggs95@users.noreply.github.com>
2018-05-02 04:29:17 +0000

    Update migration.rb
    Having self.change was incorrect.

--- lib/generators/ratyrate/templates/migration.rb
+++ lib/generators/ratyrate/templates/migration.rb
@@ -1,5 +1,5 @@
 class CreateRates < ActiveRecord::Migration[5.1]
-  def self.change
+  def change
     create_table :rates do |t|
       t.belongs_to :rater
       t.belongs_to :rateable, :polymorphic => true
